2803c1347fb2b3dd19e60d2fc9aa963d7c0beb6f
Ian de Oliveira <94008235+ian-oliveira@users.noreply.github.com>
2022-06-22 12:44:10 -0300

    database seeding

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -6,10 +6,23 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication  {
 
+	/*
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	*/
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
+
+	/*
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
+		
+	}
+	*/
 
 }
